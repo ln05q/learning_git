771c0399bd6c9643e0bfb3780e8d279edaedb427
lenhatquang <lenhatquang11a1@gmail.com>
2025-03-01 17:37:21 +0700

    create new verilog file

---
+++ decoder_4to1.v
@@ -0,0 +1,11 @@
+module decoder_4to1.v(s1, s0, i3, i2, i1, i0, out); //
+
+input s1, s0;
+
+input [3:0] i3, i2, i1, i0;
+output [3:0] out;
+
+
+assign out = s1 ? (s0 ? i3 : i2) : (s0 ? i1 : i0);
+
+endmodule
